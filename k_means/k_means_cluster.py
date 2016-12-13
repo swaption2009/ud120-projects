a14d42df09885772b574640736167aa643ddf13d
swaption2009 <swaption2009@gmail.com>
2016-12-13 11:52:25 -0800

    k_means and feature scaling

--- k_means/k_means_cluster.py
+++ k_means/k_means_cluster.py
@@ -48,29 +48,42 @@ data_dict.pop("TOTAL", 0)
 ### can be any key in the person-level dictionary (salary, director_fees, etc.) 
 feature_1 = "salary"
 feature_2 = "exercised_stock_options"
+feature_3 = "total_payments"
 poi  = "poi"
-features_list = [poi, feature_1, feature_2]
+features_list = [poi, feature_1, feature_2, feature_3]
 data = featureFormat(data_dict, features_list )
 poi, finance_features = targetFeatureSplit( data )
-
 
 ### in the "clustering with 3 features" part of the mini-project,
 ### you'll want to change this line to 
 ### for f1, f2, _ in finance_features:
 ### (as it's currently written, the line below assumes 2 features)
-for f1, f2 in finance_features:
+for f1, f2, f3 in finance_features:
     plt.scatter( f1, f2 )
 plt.show()
 
 ### cluster here; create predictions of the cluster labels
 ### for the data and store them to a list called pred
+from sklearn.cluster import KMeans
+
+classifier = KMeans(n_clusters=2)
+classifier.fit(finance_features)
+pred = classifier.predict(finance_features)
 
 
+# FEATURE SCALING
 
+exercised_stock_options = [item["exercised_stock_options"] for k, item in data_dict.iteritems() if not item["exercised_stock_options"] == "NaN"]
+print "exercised stock options min is %s" % min(exercised_stock_options)
+print "exercised stock options max is %s" % max(exercised_stock_options)
+
+salary = [item["salary"] for k, item in data_dict.iteritems() if not item["salary"] == "NaN"]
+print "salary min is %s" % min(salary)
+print "salary max is %s" % max(salary)
 
 ### rename the "name" parameter when you change the number of features
 ### so that the figure gets saved to a different file
 try:
-    Draw(pred, finance_features, poi, mark_poi=False, name="clusters.pdf", f1_name=feature_1, f2_name=feature_2)
+    Draw(pred, finance_features, poi, mark_poi=False, name="clusters_3 features.pdf", f1_name=feature_1, f2_name=feature_2)
 except NameError:
     print "no predictions object named pred found, no clusters to plot"
